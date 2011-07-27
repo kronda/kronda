05048f581ec4620589e7a41788cc3fd7ecb4153a
Kronda <ephany@gmail.com>
2011-07-27 16:26:10 -0700

    added change to scripts.js and added gitignore file

--- wp-content/themes/kronda/js/scripts.js
+++ wp-content/themes/kronda/js/scripts.js
@@ -38,8 +38,37 @@ jQuery(document).ready(function($) {
 		return $('#blogteaser').height() + 10 + 'px';
 	});
 
-  alert(jQuery.browser.version);
+  //alert(jQuery.browser.version);
   if (jQuery.browser.version == 7.0) {
     alert('What are ya trapped at work with some old legacy office computer? Upgrade to IE8 or 9 why don\'tcha?');
-  };
+  }
+
+	//noise for background
+	function generateNoise (opacity) {
+    if (!!!document.createElement('canvas').getContext ) {
+      return false;
+    }
+    var canvas = document.createElement('canvas'),
+      ctx = canvas.getContext('2d'),
+      x, y,
+      r, g, b,
+      opacity = opacity || 0.2;
+      
+      canvas.width = 55;
+      canvas.height = 55;
+      
+    for ( x = 0; x < canvas.width; x++ ) {
+      for ( y = 0; y < canvas.height; y++ ) {
+        r = Math.floor( Math.random() * 255 );
+        g = Math.floor( Math.random() * 255 );
+        b = Math.floor( Math.random() * 255 );
+        
+        ctx.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity + ')';
+        ctx.fillRect(x, y, 1, 1);
+      }
+    }
+    
+    document.getElementById('wrapper').style.backgroundImage = "url(" + canvas.toDataURL("image/png") + ")";
+  }
+  generateNoise(0.07);
 });
