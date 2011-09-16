e102aac19bead99dd802b2a194e3d7bc20cc2e5f
Kronda <ephany@gmail.com>
2011-09-16 08:28:46 -0700

    added viewport meta tag to fix expanding when going to landscape

--- wp-content/themes/kronda/header.php
+++ wp-content/themes/kronda/header.php
@@ -24,6 +24,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+<meta name="viewport" content="initial-scale=1, maximum-scale=1">
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
