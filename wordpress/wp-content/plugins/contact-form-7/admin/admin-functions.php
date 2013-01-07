c9baf87b843847052ce08658e4364b4cfc2d54d6
Kronda <ephany@gmail.com>
2013-01-07 12:49:41 -0800

    updated all plugins

--- wordpress/wp-content/plugins/contact-form-7/admin/admin-functions.php
+++ wordpress/wp-content/plugins/contact-form-7/admin/admin-functions.php
@@ -1,5 +1,19 @@
 <?php
 
+function wpcf7_current_action() {
+	if ( isset( $_REQUEST['action'] ) && -1 != $_REQUEST['action'] )
+		return $_REQUEST['action'];
+
+	if ( isset( $_REQUEST['action2'] ) && -1 != $_REQUEST['action2'] )
+		return $_REQUEST['action2'];
+
+	return false;
+}
+
+function wpcf7_admin_has_edit_cap() {
+	return current_user_can( 'wpcf7_edit_contact_forms' );
+}
+
 function wpcf7_add_tag_generator( $name, $title, $elm_id, $callback, $options = array() ) {
 	global $wpcf7_tag_generators;
 
@@ -21,29 +35,18 @@ function wpcf7_add_tag_generator( $name, $title, $elm_id, $callback, $options = 
 	return true;
 }
 
-function wpcf7_print_tag_generators() {
+function wpcf7_tag_generators() {
 	global $wpcf7_tag_generators;
 
-	$output = array();
+	$taggenerators = array();
 
 	foreach ( (array) $wpcf7_tag_generators as $name => $tg ) {
-		$pane = "		" . esc_js( $name ) . ": { ";
-		$pane .= "title: '" . esc_js( $tg['title'] ) . "'";
-		$pane .= ", content: '" . esc_js( $tg['content'] ) . "'";
-
-		foreach ( (array) $tg['options'] as $option_name => $option_value ) {
-			if ( is_int( $option_value ) )
-				$pane .= ", $option_name: $option_value";
-			else
-				$pane .= ", $option_name: '" . esc_js( $option_value ) . "'";
-		}
-
-		$pane .= " }";
-
-		$output[] = $pane;
+		$taggenerators[$name] = array_merge(
+			(array) $tg['options'],
+			array( 'title' => $tg['title'], 'content' => $tg['content'] ) );
 	}
 
-	echo implode( ",\n", $output ) . "\n";
+	return $taggenerators;
 }
 
 ?>
