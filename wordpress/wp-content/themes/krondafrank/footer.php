41a8a4b64cc71b675180acaeb4b052d30374932f
Kronda <ephany@gmail.com>
2013-02-08 13:21:16 -0800

    added krondafrank theme

---
+++ wordpress/wp-content/themes/krondafrank/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package Frank
+ */
+?>
+</div>
+<?php if ( is_active_sidebar("widget-footer") ) : ?>
+<div id="page-bottom">
+	<footer id='page-footer' class='container'>	
+		<div class="row">
+		<?php if ( !dynamic_sidebar("Footer") ) : ?>
+			
+		<?php endif; ?>
+		</div>
+	</footer>
+</div>
+<?php endif; ?>
+<?php wp_footer(); ?>
+<!--<?php echo get_num_queries(); ?> queries in <?php timer_stop(1); ?> seconds.-->
+</body>
+</html>
