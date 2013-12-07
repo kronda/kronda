8aef4d60283c12a3b828f30ecdeebf7c7a253aea
Kronda <ephany@gmail.com>
2013-12-06 19:03:01 -0800

    deleted extra themes

---
+++ wordpress/wp-content/themes/reverie/footer.php
@@ -0,0 +1,27 @@
+	</div><!-- Row End -->
+</section><!-- Container End -->
+
+<div class="row full-width">
+	<?php dynamic_sidebar("Footer"); ?>
+</div>
+
+<footer class="row full-width" role="contentinfo">
+	<div class="small-12 large-4 columns">
+		<p>&copy; <?php echo date('Y'); ?>. <?php _e('Crafted on','reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
+	</div>
+	
+	<div class="small-12 large-8 columns">
+		<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list right')); ?>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+
+<script>
+	(function($) {
+		$(document).foundation();
+	})(jQuery);
+</script>
+	
+</body>
+</html>
---
+++ wordpress/wp-content/themes/reverie/header.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" <?php language_attributes(); ?> > <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" <?php language_attributes(); ?> > <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?> "> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?> > <!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+
+	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+
+	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
+	<meta name="viewport" content="width=device-width" />
+
+	<!-- Favicon and Feed -->
+	<link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+
+	<!--  iPhone Web App Home Screen Icon -->
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-ipad.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-retina.png" />
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon.png" />
+
+	<!-- Enable Startup Image for iOS Home Screen Web App -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/mobile-load.png" />
+
+	<!-- Startup Image iPad Landscape (748x1024) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
+	<!-- Startup Image iPad Portrait (768x1004) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
+	<!-- Startup Image iPhone (320x460) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load.png" media="screen and (max-device-width: 320px)" />
+
+<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="contain-to-grid">
+	<!-- Starting the Top-Bar -->
+	<nav class="top-bar">
+	    <ul class="title-area">
+	        <li class="name">
+	        	<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	        </li>
+			<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+			<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+	    </ul>
+	    <section class="top-bar-section">
+	    <?php
+	        wp_nav_menu( array(
+	            'theme_location' => 'primary',
+	            'container' => false,
+	            'depth' => 0,
+	            'items_wrap' => '<ul class="left">%3$s</ul>',
+	            'fallback_cb' => 'reverie_menu_fallback', // workaround to show a message to set up a menu
+	            'walker' => new reverie_walker( array(
+	                'in_top_bar' => true,
+	                'item_type' => 'li',
+	                'menu_type' => 'main-menu'
+	            ) ),
+	        ) );
+	    ?>
+	    <ul class="right">
+	    	<li class="divider hide-for-small"></li>
+	    	<li class="has-form"><?php get_search_form(); ?></li>
+	    </ul>
+	    </section>
+	</nav>
+	<!-- End of Top-Bar -->
+</div>
+
+<header class="row" role="banner">
+	<div class="small-12 columns">
+		<h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
+		<h4 class="subheader"><?php bloginfo('description'); ?></h4>
+		<hr/>
+	</div>
+</header>
+
+<!-- Start the main container -->
+<section class="container" role="document">
+	<div class="row">
