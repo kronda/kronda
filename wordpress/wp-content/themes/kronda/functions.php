a1060e1416d31d43381bb47468489ea072e09d4a
Kronda <ephany@gmail.com>
2014-01-29 19:49:49 -0800

    added fb open graph to functions.php

--- wordpress/wp-content/themes/kronda/functions.php
+++ wordpress/wp-content/themes/kronda/functions.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * child functions and definitions
+ * kronda functions and definitions
  *
- * @package child
+ * @package kronda
  */
 
 /**
  * Clean up the output of the <head> block
  */
-function child_clean_header() {
+function kronda_clean_header() {
   remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
   remove_action( 'wp_head', 'feed_links', 2 );       // Display the links to the general feeds: Post and Comment Feed
   remove_action( 'wp_head', 'rsd_link' );            // Display the link to the Really Simple Discovery service endpoint, EditURI link
@@ -16,16 +16,16 @@ function child_clean_header() {
   remove_action( 'wp_head', 'index_rel_link' );      // index link
   remove_action( 'wp_head', 'wp_generator' );        // Display the XHTML generator that is generated on the wp_head hook, WP version
 }
-add_action( 'init', 'child_clean_header' );
+add_action( 'init', 'kronda_clean_header' );
 
-function child_nofollow_cat_posts($text) {
+function kronda_nofollow_cat_posts($text) {
 global $post;
         if( in_category(1) ) { // SET CATEGORY ID HERE
                 $text = stripslashes(wp_rel_nofollow($text));
         }
         return $text;
 }
-add_filter('the_content', 'child_nofollow_cat_posts');
+add_filter('the_content', 'kronda_nofollow_cat_posts');
 
 function kronda_add_body_class( $classes ) {
   global $post;
@@ -39,4 +39,10 @@ function kronda_add_body_class( $classes ) {
 }
 
 add_filter( 'body_class', 'kronda_add_body_class' );
- 
+
+function kronda_add_facebook_meta_tags() {
+  echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" />';
+  echo '<meta property="og:title" content="' . bloginfo('name') . '"/>';
+}
+
+add_action('wp_head', 'kronda_add_facebook_meta_tags');
